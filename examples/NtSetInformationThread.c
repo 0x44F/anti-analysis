0bd5d3cd89fee05fad3572fe1286674bbcefaab1
0x44F <89411010+0x44F@users.noreply.github.com>
2023-08-10 15:09:38 +1200

    Fix: consistent code, NtSetInformationThread anti-debug method

--- examples/NtSetInformationThread.c
+++ examples/NtSetInformationThread.c
@@ -5,11 +5,11 @@ bool __AmIBeingDebugged()
 {
     HMODULE ntdll = GetModuleHandleA("ntdll.dll");
     if (ntdll == NULL)
-        return 0;
+        return false;
 
     NtSetInformationThread_t NtSetInformationThread = (NtSetInformationThread_t)GetProcAddress(ntdll, "NtSetInformationThread");
     if (NtSetInformationThread == NULL)
-        return 0;
+        return false;
 
     NTSTATUS status = NtSetInformationThread(GetCurrentThread(), 0x11, 0, 0);
     if (status == 0xC0000022)
